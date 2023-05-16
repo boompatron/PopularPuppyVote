ddb57c037110b88d4fdfcecacf410a06b42fce03
boompatron <grapeflavoredapple@gmail.com>
2023-05-16 23:59:41 +0900

    Initial commit

---
+++ src/main/java/com/numble/popularpuppyvote/PopularPuppyVoteApplication.java
@@ -0,0 +1,13 @@
+package com.numble.popularpuppyvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularPuppyVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopularPuppyVoteApplication.class, args);
+	}
+
+}
