e731c44fa276e72496f18b55fc8881beb406cfe4
boompatron <grapeflavoredapple@gmail.com>
2023-05-19 00:36:43 +0900

    [PPV-6] Feat: Puppy 단일 조회 기능 구현

--- src/main/java/com/numble/popularpuppyvote/domain/puppy/controller/PuppyController.java
+++ src/main/java/com/numble/popularpuppyvote/domain/puppy/controller/PuppyController.java
@@ -4,14 +4,20 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.numble.popularpuppyvote.domain.puppy.dto.request.PuppyListGetRequest;
 import com.numble.popularpuppyvote.domain.puppy.dto.request.PuppyCreateRequest;
+import com.numble.popularpuppyvote.domain.puppy.dto.response.PuppyListGetResponse;
 import com.numble.popularpuppyvote.domain.puppy.dto.response.PuppyCreateResponse;
+import com.numble.popularpuppyvote.domain.puppy.dto.response.PuppyGetResponse;
+import com.numble.popularpuppyvote.domain.puppy.service.PuppyReadService;
 import com.numble.popularpuppyvote.domain.puppy.service.PuppyService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,13 +28,30 @@ import lombok.RequiredArgsConstructor;
 public class PuppyController {
 
 	private final PuppyService puppyService;
+	private final PuppyReadService puppyReadService;
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public ResponseEntity<PuppyCreateResponse> registerPuppy(
 			@RequestBody @Valid PuppyCreateRequest request
-	){
-		return ResponseEntity.ok(puppyService.registerPuppy(request));
+	) {
+		return new ResponseEntity<>(puppyService.registerPuppy(request), HttpStatus.CREATED);
+	}
+
+	@GetMapping("/{puppyId}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public ResponseEntity<PuppyGetResponse> getOnePuppy(
+			@PathVariable Long puppyId
+	) {
+		return ResponseEntity.ok(puppyReadService.getOnePuppy(puppyId));
+	}
+
+	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
+	public ResponseEntity<PuppyListGetResponse> getPuppies(
+			@Valid PuppyListGetRequest request
+	) {
+		return ResponseEntity.ok(puppyReadService.getPuppies(request));
 	}
 
 }
---
+++ src/main/java/com/numble/popularpuppyvote/domain/puppy/dto/request/PuppyGetRequest.java
@@ -0,0 +1,6 @@
+package com.numble.popularpuppyvote.domain.puppy.dto.request;
+
+public record PuppyGetRequest(
+		Long id
+) {
+}
