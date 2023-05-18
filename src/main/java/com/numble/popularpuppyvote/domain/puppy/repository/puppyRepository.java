cb791ae3d029dd5380a08d2b2246b6c690ecf92d
boompatron <grapeflavoredapple@gmail.com>
2023-05-18 17:30:03 +0900

    Feat: PuppyRepository 생성

---
+++ src/main/java/com/numble/popularpuppyvote/domain/puppy/repository/puppyRepository.java
@@ -0,0 +1,11 @@
+package com.numble.popularpuppyvote.domain.puppy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.numble.popularpuppyvote.domain.puppy.model.Puppy;
+
+@Repository
+public interface puppyRepository extends JpaRepository<Puppy, Long> {
+}
+성
