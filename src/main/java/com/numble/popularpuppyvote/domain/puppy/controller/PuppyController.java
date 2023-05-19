6445c737a0f437d82630170ab91bbde7d2dbe412
boompatron <grapeflavoredapple@gmail.com>
2023-05-19 14:38:42 +0900

    [PPV-9] Feat: Puppy 수정 기능 구현

--- src/main/java/com/numble/popularpuppyvote/domain/puppy/controller/PuppyController.java
+++ src/main/java/com/numble/popularpuppyvote/domain/puppy/controller/PuppyController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.numble.popularpuppyvote.domain.puppy.dto.request.PuppyListGetRequest;
 import com.numble.popularpuppyvote.domain.puppy.dto.request.PuppyCreateRequest;
+import com.numble.popularpuppyvote.domain.puppy.dto.request.PuppyUpdateRequest;
 import com.numble.popularpuppyvote.domain.puppy.dto.response.PuppyListGetResponse;
 import com.numble.popularpuppyvote.domain.puppy.dto.response.PuppyCreateResponse;
 import com.numble.popularpuppyvote.domain.puppy.dto.response.PuppyGetResponse;
+import com.numble.popularpuppyvote.domain.puppy.dto.response.PuppyUpdateResponse;
 import com.numble.popularpuppyvote.domain.puppy.service.PuppyReadService;
 import com.numble.popularpuppyvote.domain.puppy.service.PuppyService;
 
@@ -54,4 +57,11 @@ public class PuppyController {
 		return ResponseEntity.ok(puppyReadService.getPuppies(request));
 	}
 
+	@PatchMapping
+	@ResponseStatus(code = HttpStatus.OK)
+	public ResponseEntity<PuppyUpdateResponse> updatePuppy(
+			@RequestBody PuppyUpdateRequest request
+	){
+		return ResponseEntity.ok(puppyService.updatePuppy(request));
+	}
 }
