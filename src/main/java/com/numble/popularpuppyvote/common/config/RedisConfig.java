b5b5beabf5efc4f9366e8e79c4ecdf3b9511b525
boompatron <grapeflavoredapple@gmail.com>
2023-05-23 17:23:11 +0900

    [PPV-22] Config: Puppy 단건 조회시 레디스를 먼저 조회하도록 설정

--- src/main/java/com/numble/popularpuppyvote/common/config/RedisConfig.java
+++ src/main/java/com/numble/popularpuppyvote/common/config/RedisConfig.java
@@ -1,32 +1,36 @@
 package com.numble.popularpuppyvote.common.config;
-//
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.data.redis.connection.RedisConnectionFactory;
-// import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-// import org.springframework.data.redis.core.RedisTemplate;
 
-// @Configuration
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.numble.popularpuppyvote.domain.dto.response.PuppyGetResponse;
+
+@Configuration
 public class RedisConfig {
-//
-// 	@Value("${spring.redis.host}")
-// 	private String host;
-//
-// 	@Value("${spring.redis.port}")
-// 	private int port;
-//
-// 	@Bean
-// 	public RedisConnectionFactory redisConnectionFactory() {
-// 		return new LettuceConnectionFactory(host, port);
-// 	}
-//
-// 	@Bean
-// 	public RedisTemplate<?, ?> redisTemplate() {
-// 		RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
-// 		redisTemplate.setConnectionFactory(redisConnectionFactory());
-// 		return redisTemplate;
-// 	}
-//
-//
+
+	@Value("${spring.redis.host}")
+	private String host;
+
+	@Value("${spring.redis.port}")
+	private int port;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(host, port);
+	}
+
+	@Bean
+	public RedisTemplate<String, PuppyGetResponse> redisTemplate() {
+		RedisTemplate<String, PuppyGetResponse> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(PuppyGetResponse.class));
+		return redisTemplate;
+	}
 }
