5f881fc4f250b2c5cc0564b83b713c0da9f6da4c
boompatron <grapeflavoredapple@gmail.com>
2023-08-15 23:54:42 +0900

    [PPV-45] Docs: Likes 도메인 DTO 문서화

--- src/main/java/com/numble/popularpuppyvote/domain/dto/request/LikesRegisterRequest.java
+++ src/main/java/com/numble/popularpuppyvote/domain/dto/request/LikesRegisterRequest.java
@@ -1,7 +1,14 @@
 package com.numble.popularpuppyvote.domain.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "좋아요 등록 요청")
 public record LikesRegisterRequest(
-		Long puppyId,
-		String sessionId
+
+	@Schema(description = "좋아요를 누른 강아지 식별자", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+	Long puppyId,
+
+	@Schema(description = "좋아요를 누른 사용자의 세션 ID", example = "exampleSessionId", requiredMode = Schema.RequiredMode.REQUIRED)
+	String sessionId
 ) {
 }
--- src/main/java/com/numble/popularpuppyvote/domain/dto/response/LikesCountGetResponse.java
+++ src/main/java/com/numble/popularpuppyvote/domain/dto/response/LikesCountGetResponse.java
@@ -1,6 +1,11 @@
 package com.numble.popularpuppyvote.domain.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "강아지 좋아요 수 응답")
 public record LikesCountGetResponse(
-		int count
+
+	@Schema(description = "강아지 좋아요 수", example = "34234", requiredMode = Schema.RequiredMode.REQUIRED)
+	int count
 ) {
 }
